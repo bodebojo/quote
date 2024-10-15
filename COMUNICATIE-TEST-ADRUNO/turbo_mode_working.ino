5788062bdcf500c1f5171fc414cb125fbdc071f7
marnexhagreis <60065029+marnexhagreis@users.noreply.github.com>
2024-10-15 11:38:10 +0200

    Update turbo_mode_working.ino

--- COMUNICATIE-TEST-ADRUNO/turbo_mode_working.ino
+++ COMUNICATIE-TEST-ADRUNO/turbo_mode_working.ino
@@ -2,37 +2,37 @@
 #include <SoftwareSerial.h> // For software serial communication if needed
 SoftwareSerial espSerial(13, 15); // Adjust pins if necessary
 int speed = 500;
-bool shift_pressed = false;
 
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication with the computer
   espSerial.begin(9600); // Initialize serial communication with the ESP`
-  setLed(255, 0, 0);
+  
 }
 
 void loop() {
   // Check if there is incoming data on the serial
   if (Serial.available()) {
-    String incomingData = Serial.readStringUntil('\n'); // Read the incoming data as a string until newline
+    // Read the incoming data as a string
+    String incomingData = Serial.readStringUntil('\n');
     incomingData.trim(); // Remove any trailing whitespace
-    Serial.println(speed); // Print the speed variable for debug purposes
-    Serial.println(incomingData); // Print the received data
+    // Print the received data
+    Serial.println(incomingData);
     // Set speed based on shift input
+    
+    
     if (incomingData == "Shift key pressed") {
-      if (shift_pressed == false) {
-        speed = 1000;
-        shift_pressed = true;
-        setLed(0, 255, 0);
-      }
-      else if (shift_pressed == true) {
-        speed = 500;
-        shift_pressed = false;
-        setLed(255, 0, 0);
-      }
+      speed = 500;
+      setLed(0, 255, 0);
+      Serial.println(speed);
+    }
+    else if (incomingData == "Shift key released") {
+      speed = 250;
+      setLed(0, 0, 255);
+      Serial.println(speed);
     }
     // Compare the incoming data
-    if (incomingData == "W key pressed") {
+    else if (incomingData == "W key pressed") {
       setMotor(10, speed);
       setMotor(9, speed);
       Serial.println("Received command1");
@@ -48,7 +48,19 @@ void loop() {
     else if (incomingData == "Z key pressed") {
       setMotor(10, speed/2.5);
       setMotor(9, -speed/2.5);
-    } 
+    }
+    else if (incomingData == "Shift key pressed") {
+      speed = 500;
+      Serial.println(speed);
+      setLed(0, 255, 0);
+      
+    }
+    else if (incomingData == "Shift key released") {
+      speed = 250;
+      Serial.println(speed);
+      setLed(0, 0, 255);
+      
+    }
     else{ 
       setMotor(9, 0);
       setMotor(10, 0);    
