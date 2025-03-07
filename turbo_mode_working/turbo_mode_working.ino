e48e536faabf8966cd2b87215f2254f76c186f40
bodebojo <bodebojo@gmail.com>
2025-03-07 12:07:21 +0100

    Added 2nd movement option meant for community-day where it drivesbased on distance to walls. Also added 3rd option where it jst simply spins;Request by Vic

--- turbo_mode_working/turbo_mode_working.ino
+++ turbo_mode_working/turbo_mode_working.ino
@@ -2,13 +2,18 @@
 #include <SoftwareSerial.h> // For software serial communication if needed
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <time.h>
 #include <string.h>
 #include <stdlib.h>
 
 SoftwareSerial espSerial(13, 15); // Adjust pins if necessary
 LiquidCrystal_I2C lcd(0x3F,20,4);
 
+bool do_boot = false;
 int speed = 500;
+int right_speed = 0;
+int left_speed = 0;
+int default_speed = 100;
 double avg_dis;  // Declaration of variable 'avg_dis' to store the average
 
 byte block[8] = {
@@ -66,12 +71,7 @@ void boot(int boot_time_s) {
   int boot_time_half_s = boot_time_s /2;
   int boot_time_half_ms = boot_time_half_s * 1000;
   int boot_delay = boot_time_half_ms/ 17;
-  int randomIndex = random(0, 5);
 
-  String faces[] = {">:3", ":0", ":p", ":D", ">:D"};
-  String randomFace = faces[randomIndex];
-
-  lcd.backlight();
   lcd.clear();
   lcd.setCursor(0,0);
 
@@ -99,7 +99,7 @@ void boot(int boot_time_s) {
 void setup() {
   Serial.begin(9600); // Initialize serial communication with the computer
   espSerial.begin(9600); // Initialize serial communication with the ESP`
-  randomSeed(analogRead(0));
+  randomSeed(time(0));
 
   lcd.init();
   lcd.createChar(0, block);
@@ -107,65 +107,181 @@ void setup() {
   lcd.createChar(2, heart1);
   lcd.createChar(3, heart2);
   lcd.createChar(4, heart3);
-
+  lcd.backlight();
+  if (do_boot == true) {
   boot(2.5);
-  
+  }
 }
 
-void move() {
-  // Check if there is incoming data on the serial
-  if (Serial.available()) {
-    // Read the incoming data as a string
-    String incomingData = Serial.readStringUntil('\n');
-    incomingData.trim(); // Remove any trailing whitespace
-    // Print the received data
-    Serial.println(incomingData);
-    // Set speed based on shift input
-    
-    if (incomingData == "Shift key pressed") {
-      speed = 500;
-      setLed(0, 255, 0);
-      Serial.println(speed);
-    }
-    if (incomingData == "Shift key released") {
-      speed = 250;
-      setLed(0, 0, 255);
-      Serial.println(speed);
-    }
-    // Compare the incoming data
-    if (incomingData == "W key pressed") {
-      setMotor(10, speed);
-      setMotor(9, speed);
-      Serial.println("Received command1");
-    }
-    if (incomingData == "Y key pressed") {
-      setMotor(10, -speed);
-      setMotor(9, -speed);
-    }
-    if (incomingData == "X key pressed") {
-      setMotor(10, -speed/2.5);
-      setMotor(9, speed/2.5);
-    } 
-    if (incomingData == "Z key pressed") {
-      setMotor(10, speed/2.5);
-      setMotor(9, -speed/2.5);
-    }
-    if (incomingData == "Shift key pressed") {
-      speed = 500;
-      Serial.println(speed);
-      setLed(0, 255, 0);
+void move(int mode) {
+  Serial.println(mode);
+  String incomingData = Serial.readStringUntil('\n');
+  incomingData.trim();
+
+  if (incomingData == "CDM1") {
+    mode = 1;
+  }
+  if (incomingData == "CDM2") {
+    mode = 2;
+  }
+  if (incomingData == "CDM3") {
+    mode = 3;
+  }
+
+  if (mode == 1) {
+    // Check if there is incoming data on the serial
+    if (Serial.available()) {
+      // Read the incoming data as a string
+      String incomingData = Serial.readStringUntil('\n');
+      incomingData.trim(); // Remove any trailing whitespace
+      // Print the received data
+      Serial.println(incomingData);
+      // Set speed based on shift input
       
+      if (incomingData == "Shift key pressed") {
+        speed = 500;
+        setLed(0, 255, 0);
+        Serial.println(speed);
+      }
+      if (incomingData == "Shift key released") {
+        speed = 250;
+        setLed(0, 0, 255);
+        Serial.println(speed);
+      }
+      // Compare the incoming data
+      if (incomingData == "W key pressed") {
+        setMotor(10, speed);
+        setMotor(9, speed);
+      }
+      if (incomingData == "S key pressed") {
+        setMotor(10, -speed);
+        setMotor(9, -speed);
+      }
+      if (incomingData == "A key pressed") {
+        setMotor(10, -speed/2.5);
+        setMotor(9, speed/2.5);
+      } 
+      if (incomingData == "D key pressed") {
+        setMotor(10, speed/2.5);
+        setMotor(9, -speed/2.5);
+      }
+      if (incomingData == "Shift key pressed") {
+        speed = 500;
+        Serial.println(speed);
+        setLed(0, 255, 0);
+        
+      }
+      if (incomingData == "Shift key released") {
+        speed = 250;
+        Serial.println(speed);
+        setLed(0, 0, 255);
+        
+      }
+      // else{ 
+      //   setMotor(9, 0);
+      //   setMotor(10, 0);    
+      // }
     }
-    if (incomingData == "Shift key released") {
-      speed = 250;
-      Serial.println(speed);
-      setLed(0, 0, 255);
-      
-    }
-    else{ 
+  }
+  if (mode == 2) {
+    int right_line_value = digitalRead(16); // Read the value of the right line follower sensor.
+    int left_line_value = digitalRead(17);  // Read the value of the left line follower sensor.
+    // 0 is detecting black, 1 is detecting white
+    if (avg_dis <= 10) {
+      setMotor(10, 0);
       setMotor(9, 0);
-      setMotor(10, 0);    
     }
+    if (right_speed == 0) {
+      right_speed = default_speed;
+      Serial.println("Setting default speed for right motor");
+    }
+    if (left_speed == 0) {
+      left_speed = default_speed;
+      Serial.println("Setting default speed for left motor");
+    }
+    if (default_speed > 25) {
+      // if (default_speed == 100) {
+      //   setMotor(10, 100);
+      //   setMotor(9, 100);
+      //   default_speed = 50;
+      // }
+      Serial.println("Default Speed (" + String(default_speed) + ") is too high decreasing by 5");
+      default_speed = default_speed - 5;
+    }
+
+
+    if (right_line_value == 1 && left_line_value == 1) {
+      setMotor(10, right_speed);
+      setMotor(9, left_speed);
+    }
+    else if (right_line_value == 1 && left_line_value == 0) {
+      right_speed = right_speed / 2;
+      left_speed = default_speed;
+      setMotor(10, right_speed);
+      setMotor(9, left_speed); 
+    }
+    else if (right_line_value == 0 && left_line_value == 1) {
+      left_speed = left_speed / 2;
+      right_speed = default_speed;
+      setMotor(10, right_speed);
+      setMotor(9, left_speed); 
+    }
+    else {
+      setMotor(10, default_speed);
+      setMotor(9, default_speed); 
+    }
+  }
+  if (mode == 3) {
+    int SPEED = 1250;
+    int loop_amount = 10;
+    int loop_count = 0;
+    int i = 0;
+    int randomIndex = random(-1, 2);
+    int stuck = 0; 
+
+    for (i = 1; i <= loop_amount; i++) {
+      get_distance();
+      
+      if (avg_dis <= 30) {
+        stuck += 1;
+        Serial.println("Stuck: "+ String(stuck, 3));
+        setMotor(10, -100);
+        setMotor(9, -100);
+        if (stuck < 10) {
+          delay(250);
+        }
+        if (stuck >= 11) {
+          delay(500);
+        }
+
+        if (randomIndex == 0) {
+          Serial.println("Random: 0");
+          setMotor(10, -100);
+          setMotor(9, 100);
+        }
+        if (randomIndex == 1) {
+          Serial.println("Random: 1");
+          setMotor(10, 100);
+          setMotor(9, -100);
+        }
+        delay(250);
+        if (avg_dis >= 30) {
+          setMotor(10, 100);
+          setMotor(9, 100);
+        }
+      }
+
+      else {
+        setMotor(10, SPEED);
+        setMotor(9, SPEED);
+      }
+    }
+  }
+  if (mode == 4) {
+    int SPEED = 500;
+
+    setMotor(10, SPEED);
+    setMotor(9, SPEED/4);
   }
 }
 
@@ -177,17 +293,17 @@ void get_distance() {
   for (i = 1; i <= loop_amount; i++) {
     n = getDistance(); // Get the distance from the sensor
     if (prev_n == 0) {prev_n = n;}
-    n = constrain(n, (prev_n-5), (prev_n+5)); // Limit the distance
+    n = constrain(n, (prev_n-50), (prev_n+50)); // Limit the distance
     sum += n; // Add the distance to the sum
     prev_n = n; // Record the old distance in prev_n
-    delay(25);
   }
   loop_count++;
   avg_dis = sum / loop_amount; // Calculate the average distance
-  print_to_lcd("AVG Distance:", String(avg_dis, 3), 0.25);
+
+  print_to_lcd("AVG Distance:", String(avg_dis, 3), 0.1, false);
 
   if (loop_count = 10) {
-    // Serial.println("Average distance: ");
+    Serial.println("Average distance: "+String(avg_dis, 3));
     // Serial.println(avg_dis); // Print the average distance to the serial monitor
     loop_count = 0;
   }
@@ -195,32 +311,44 @@ void get_distance() {
   sum = 0;
 }
 
-void get_line_information() {
-
-  int rightLineValue = digitalRead(16); // Read the value of the right line follower sensor.
-  int leftLineValue = digitalRead(17);  // Read the value of the left line follower sensor.
-
-  if (leftLineValue == 0 && rightLineValue == 0)
-  {
-      // The robot is on the line.
-      Serial.println("The robot is on the line.");
-  }
-  else if (leftLineValue == 0 && rightLineValue == 1)
-  {
-      // The robot is to the left of the line.
-      Serial.println("The robot is to the left of the line.");
-  }
-  else if (leftLineValue == 1 && rightLineValue == 0)
-  {
-      // The robot is to the right of the line.
-      Serial.println("The robot is to the right of the line.");
-  }
-  else {
-    Serial.println("The robot is flying in space and cant see a line no matter how far it looks.");
-  }
+String pickRandomString(String stringList[], int listSize) {
+  int randomIndex = random(listSize);
+  return stringList[randomIndex];
 }
 
-void print_to_lcd(String first_line, String second_line, int pause) {
+void show_faces() {
+  // lcd.autoscroll();
+  // String faces[] = {">:3", ":0", ":p", ":D", ">:D"};
+  // int randomIndex0 = random(0, 5);
+  // String randomFace0 = faces[randomIndex0];
+  // int randomIndex1 = random(0, 5);
+  // String randomFace1 = faces[randomIndex1];
+  // String information = "The quick brown fox jumps over the lazy"; // 40 chars per line limit
+
+  String empty_string = "                ";
+
+  String eye[] = {"O", "#", "$", "*", "V", "-", "|", "Z"};
+  String eyes[] =   {"  O          0  ", "  -          O  ", "  V          V  ", "  |          |  ", "  -          -  ", "  #          #  ", "  $          $  "};
+  String mouths[] = {"       ==       ", "       ()       ", "       []       ", "       vv       ", "       ##       "};
+  
+  int eye_list_size = sizeof(eye) / sizeof(eye[0]);
+  int eyes_list_size = sizeof(eyes) / sizeof(eyes[0]);
+  int mouths_list_size = sizeof(mouths) / sizeof(mouths[0]);
+
+  String random_eye1 = pickRandomString(eyes, eyes_list_size);
+  String random_eye2 = pickRandomString(eyes, eyes_list_size);
+
+  // String random_eyes = pickRandomString(eyes, eyes_list_size);
+
+  String random_mouth = pickRandomString(mouths, mouths_list_size);
+
+  String double_eyes = ""+random_eye1+"          "+random_eye2+" ";
+
+  print_to_lcd(double_eyes, random_mouth, 2, true);
+
+}
+
+void print_to_lcd(String first_line, String second_line, int pause, bool clear) {
   float pause_ms = pause * 1000;
 
   lcd.setCursor(0,0);
@@ -229,16 +357,16 @@ void print_to_lcd(String first_line, String second_line, int pause) {
   lcd.print(second_line);
 
   delay(pause_ms);
+  if (clear == true) {
+    lcd.clear();
+  }
 }
 
 void loop() {
-  move();
-  get_distance();
+
+  move(3); //(1: Web Based, 2: Line Following, 3: Self Driving, 4: Circles ^^)
+
+  // show_faces();
 
   avg_dis = 0;
-
-  int pin16State = digitalRead(16);
-  Serial.println("Pin 16 state: " + String(pin16State));
-  int pin17State = digitalRead(17);
-  Serial.println("Pin 17 state: " + String(pin17State));  
 }
