8806b16c222cdc60f0854508c32cf0490387f078
bodebojo <bodebojo@gmail.com>
2025-05-09 12:18:15 +0200

    Added Key Down detection

--- better-shailho-code/mode1.h
+++ better-shailho-code/mode1.h
@@ -4,36 +4,63 @@
 
 extern String incomingData;
 extern int speed;
-
+extern int left_speed;
+extern int right_speed;
 
 void mode1(){
 // Set speed based on shift input
     if (incomingData == "Key Down: Shift") {
       speed = 500;
       setLed(0, 255, 0);
-  } if (incomingData == "Key Up: Shift") {
+    } 
+    if (incomingData == "Key Up: Shift") {
       speed = 250;
       setLed(0, 0, 255);
-  } if (incomingData == "Key Down: w") {
-      setMotor(10, speed);
-      setMotor(9, speed);
-      Serial.println("w ket detecd");
-  } if (incomingData == "Key Down: s") {
-      setMotor(10, -speed);
-      setMotor(9, -speed);
-  } if (incomingData == "Key Down: a") {
-      setMotor(10, -speed/2.5);
-      setMotor(9, speed/2.5);
-  } if (incomingData == "Key Down: d") {
-      setMotor(10, speed/2.5);
-      setMotor(9, -speed/2.5);
-  } if (incomingData == "Key Down: Shift") {
-      speed = 500;
-      setLed(0, 255, 0);      
-  }//else{ 
-//    setMotor(9, 0);
-//    setMotor(10, 0);    
-//}
+    } 
+    if (incomingData == "Key Down: w") {
+    left_speed =+ speed;
+    right_speed =+ speed;
+      Serial.println("W Key Down");
+    } 
+    if (incomingData == "Key Down: s") {
+    left_speed =- speed;
+    right_speed =- speed;
+      Serial.println("S Key Down");
+    } 
+    if (incomingData == "Key Down: a") {
+    left_speed =- speed;
+    right_speed =+ speed;
+      Serial.println("A Key Down");
+    } 
+    if (incomingData == "Key Down: d") {
+    left_speed =+ speed;
+    right_speed =- speed;
+      Serial.println("D Key Down");
+    }
+    if (incomingData == "Key Up: w") {
+    left_speed =- speed;
+    right_speed =- speed;
+      Serial.println("W Key Up");
+    } 
+    if (incomingData == "Key Up: s") {
+    left_speed =+ speed;
+    right_speed =+ speed;
+      Serial.println("S Key Up");
+    } 
+    if (incomingData == "Key Up: a") {
+    left_speed =+ speed;
+    right_speed =- speed;
+      Serial.println("A Key Up");
+    } 
+    if (incomingData == "Key Up: d") {
+    left_speed =- speed;
+    right_speed =+ speed;
+      Serial.println("D Key Up");
+    }
+    //else{ 
+    //    setMotor(9, 0);
+    //    setMotor(10, 0);    
+    //}
 }
 
 #endif
